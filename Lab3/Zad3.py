bac8cde1ce7aff3ad06ad030197e9ffa12abde0b
tusiaa <sandra.leman146@gmail.com>
2022-03-23 22:40:02 +0100

    Zad 3

--- Lab3/Zad3.py
+++ Lab3/Zad3.py
@@ -1,5 +1,6 @@
 import pygad
 import numpy
+import time
 
 lab = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
        [1, 2, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
@@ -16,7 +17,7 @@ lab = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 
 #definiujemy parametry chromosomu
 #geny to liczby: 0 lub 1
-gene_space = ['P', 'L', 'G', 'D']
+gene_space = [1, 2, 3, 4]
 
 #definiujemy funkcjÄ fitness
 def fitness_func(solution, solution_idx):
@@ -24,46 +25,58 @@ def fitness_func(solution, solution_idx):
     y = 1
     for i in range(30):
         match solution[i]:
-            case 'P':
+            case 1:
                 if lab[x+1][y] == 0:
                     x += 1
                 elif lab[x+1][y] == 3:
                     x += 1
-                    break
-            case 'L':
+                    for j in range(i+1, 30):
+                        solution[j] = 0
+                elif lab[x+1][y] == 1:
+                    solution[i] = 0
+            case 2:
                 if lab[x-1][y] == 0:
                     x -= 1
                 elif lab[x-1][y] == 3:
                     x -= 1
-                    break
-            case 'G':
+                    for j in range(i+1, 30):
+                        solution[j] = 0
+                elif lab[x-1][y] == 1:
+                    solution[i] = 0
+            case 3:
                 if lab[x][y-1] == 0:
                     y -= 1
                 elif lab[x][y-1] == 3:
                     y -= 1
-                    break
-            case 'D':
+                    for j in range(i+1, 30):
+                        solution[j] = 0
+                elif lab[x][y-1] == 1:
+                    solution[i] = 0
+            case 4:
                 if lab[x][y+1] == 0:
                     y += 1
                 elif lab[x][y+1] == 3:
                     y += 1
-                    break
-    fitness = 10 - x + 10 - y
+                    for j in range(i+1, 30):
+                        solution[j] = 0
+                elif lab[x][y+1] == 1:
+                    solution[i] = 0
+    fitness = -(10 - x + 10 - y)
     return fitness
 
 fitness_function = fitness_func
 
 #ile chromsomĂłw w populacji
 #ile genow ma chromosom
-sol_per_pop = 10
+sol_per_pop = 200
 num_genes = 30
 
 #ile wylaniamy rodzicow do "rozmanazania" (okolo 50% populacji)
 #ile pokolen
 #ilu rodzicow zachowac (kilka procent)
-num_parents_mating = 5
-num_generations = 30
-keep_parents = 2
+num_parents_mating = 100
+num_generations = 100
+keep_parents = 5
 
 #jaki typ selekcji rodzicow?
 #sss = steady, rws=roulette, rank = rankingowa, tournament = turniejowa
@@ -77,21 +90,32 @@ crossover_type = "single_point"
 mutation_type = "random"
 mutation_percent_genes = 4
 
-# inicjacja algorytmu z powyzszymi parametrami wpisanymi w atrybuty
-ga_instance = pygad.GA(gene_space=gene_space,
-                       num_generations=num_generations,
-                       num_parents_mating=num_parents_mating,
-                       fitness_func=fitness_function,
-                       sol_per_pop=sol_per_pop,
-                       num_genes=num_genes,
-                       parent_selection_type=parent_selection_type,
-                       keep_parents=keep_parents,
-                       crossover_type=crossover_type,
-                       mutation_type=mutation_type,
-                       mutation_percent_genes=mutation_percent_genes)
+t = 0
+for i in range(10):
 
-#uruchomienie algorytmu
-ga_instance.run()
+    # inicjacja algorytmu z powyzszymi parametrami wpisanymi w atrybuty
+    ga_instance = pygad.GA(gene_space=gene_space,
+                           num_generations=num_generations,
+                           num_parents_mating=num_parents_mating,
+                           fitness_func=fitness_function,
+                           sol_per_pop=sol_per_pop,
+                           num_genes=num_genes,
+                           parent_selection_type=parent_selection_type,
+                           keep_parents=keep_parents,
+                           crossover_type=crossover_type,
+                           mutation_type=mutation_type,
+                           mutation_percent_genes=mutation_percent_genes,
+                           stop_criteria=["reach_0"])
+
+    #uruchomienie algorytmu
+    start = time.time()
+    ga_instance.run()
+    end = time.time()
+    print(end - start)
+    t = t + (end - start)
+
+print('Średnia')
+print(t / 10)
 
 #podsumowanie: najlepsze znalezione rozwiazanie (chromosom+ocena)
 solution, solution_fitness, solution_idx = ga_instance.best_solution()
@@ -99,8 +123,18 @@ print("Parameters of the best solution : {solution}".format(solution=solution))
 print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
 
 #tutaj dodatkowo wyswietlamy sume wskazana przez jedynki
-# prediction = numpy.sum(S*solution)
-# print("Predicted output based on the best solution : {prediction}".format(prediction=prediction))
+prediction = []
+for i in solution:
+    match i:
+        case 1:
+            prediction.append("D")
+        case 2:
+            prediction.append("G")
+        case 3:
+            prediction.append("L")
+        case 4:
+            prediction.append("P")
+print("Predicted output based on the best solution : {prediction}".format(prediction=prediction))
 
 #wyswietlenie wykresu: jak zmieniala sie ocena na przestrzeni pokolen
 ga_instance.plot_fitness()
