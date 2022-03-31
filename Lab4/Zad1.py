7255b10c5f162c74f00c5106ded8487ab8ee194a
tusiaa <sandra.leman146@gmail.com>
2022-03-31 22:38:12 +0200

    Zad 1/2 check

--- Lab4/Zad1.py
+++ Lab4/Zad1.py
@@ -1,9 +1,10 @@
 import pyswarms as ps
+from matplotlib import pyplot as plt
 from pyswarms.utils.functions import single_obj as fx
 from pyswarms.utils.plotters import plot_cost_history
 
 # Set-up hyperparameters
-options = {'c1': 0.5, 'c2': 0.3, 'w':0.9}
+options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9}
 
 # Call instance of GlobalBestPSO
 optimizer = ps.single.GlobalBestPSO(n_particles=10, dimensions=2,
--- Lab4/Zad2.py
+++ Lab4/Zad2.py
@@ -4,16 +4,18 @@ from pyswarms.utils.plotters.plotters import plot_contour
 from pyswarms.utils.plotters.formatters import Mesher
 
 # Run optimizer
-options = {'c1':0.5, 'c2':0.3, 'w':0.9}
+options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9}
 optimizer = ps.single.GlobalBestPSO(n_particles=10, dimensions=2, options=options)
 
 # historia kosztów i pozycji
-cost_history, pos_history = optimizer.optimize(fx.sphere, iters=50)
+stats = optimizer.optimize(fx.sphere, iters=50)
+cost_history = optimizer.cost_history
+pos_history = optimizer.pos_history
+print(cost_history)
+print(pos_history)
 
 #tworzenie animacji
 m = Mesher(func=fx.sphere)
-animation = plot_contour(pos_history=pos_history,
- mesher=m,
-mark=(0, 0))
+animation = plot_contour(pos_history=pos_history, mesher=m, mark=(0, 0))
 animation.save('plot0.gif', writer='imagemagick', fps=10)
 
