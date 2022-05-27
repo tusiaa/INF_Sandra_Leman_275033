fdf6ecf6b776966bcc2ad7e73148188bb1212d1a
tusiaa <sandra.leman146@gmail.com>
2022-05-27 11:12:49 +0200

    Zad 2 with sklearn

--- Lab9/Zad2.py
+++ Lab9/Zad2.py
@@ -1,10 +1,9 @@
 import re
 from math import log
-
-import numpy as np
 import pandas as pd
 from nltk import word_tokenize, WordNetLemmatizer
-from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 with open('article1.txt', encoding="utf8") as f:
@@ -16,7 +15,23 @@ with open('article3.txt', encoding="utf8") as f:
 tokenized_article1 = word_tokenize(article1)
 tokenized_article2 = word_tokenize(article2)
 tokenized_article3 = word_tokenize(article3)
-n = [len(tokenized_article1), len(tokenized_article2), len(tokenized_article3)]
+stop_words = stopwords.words("english")
+
+filtered_article1 = []
+filtered_article2 = []
+filtered_article3 = []
+for w in tokenized_article1:
+    if w not in stop_words:
+        filtered_article1.append(w)
+for w in tokenized_article2:
+    if w not in stop_words:
+        filtered_article2.append(w)
+for w in tokenized_article3:
+    if w not in stop_words:
+        filtered_article3.append(w)
+
+n = [len(filtered_article1), len(filtered_article2), len(filtered_article3)]
+print(n)
 
 
 class LemmaTokenizer:
@@ -30,7 +45,7 @@ class LemmaTokenizer:
                 if not re.search(regex_num_punctuation, t) and not re.search(regex_little_words, t)]
 
 
-count_vect = CountVectorizer(stop_words='english', tokenizer=LemmaTokenizer())
+count_vect = CountVectorizer(stop_words=stop_words, tokenizer=LemmaTokenizer())
 count_matrix = count_vect.fit_transform([article1, article2, article3])
 count_array = count_matrix.toarray()
 df = pd.DataFrame(data=count_array, columns=count_vect.get_feature_names_out())
@@ -44,7 +59,7 @@ TF = df.copy()
 TF.transpose().to_csv("TF.csv")
 print("\nTF:\n", TF)
 
-count_vect = CountVectorizer(stop_words='english', tokenizer=LemmaTokenizer(), binary=True)
+count_vect = CountVectorizer(stop_words=stop_words, tokenizer=LemmaTokenizer(), binary=True)
 count_matrix = count_vect.fit_transform([article1, article2, article3])
 count_array = count_matrix.toarray()
 df = pd.DataFrame(data=count_array, columns=count_vect.get_feature_names_out())
@@ -65,3 +80,18 @@ print("\nTFIDF:\n", TFIDF)
 print("\ncosine similarity matrix:\n", cosine_similarity(TF.values))
 
 
+tf_vect = TfidfVectorizer(use_idf=False, stop_words=stop_words, tokenizer=LemmaTokenizer())
+tf_matrix = tf_vect.fit_transform([article1, article2, article3])
+tf_array = tf_matrix.toarray()
+df = pd.DataFrame(data=tf_array, columns=tf_vect.get_feature_names_out())
+print("\nTF with sklearn\n", df)
+TF2 = df.copy()
+
+tf_vect = TfidfVectorizer(stop_words=stop_words, tokenizer=LemmaTokenizer())
+tf_matrix = tf_vect.fit_transform([article1, article2, article3])
+tf_array = tf_matrix.toarray()
+df = pd.DataFrame(data=tf_array, columns=tf_vect.get_feature_names_out())
+print("\nIDF with sklearn\n", tf_vect.idf_)
+print("\nTFIDF with sklearn\n", df)
+
+print("\ncosine similarity matrix for sklearn:\n", cosine_similarity(TF2.values))
